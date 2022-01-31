73036c655b718a4137101d113d176118d60a97c6
kyzin86 <kyzin86@gmai.com>
2022-01-31 23:17:51 +0300

    delete points

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace practical_work2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello. I am Kuznecov A.E.");
-            //здесь должна быть ваша строчка...
+            //здесь должна быть ваша строчка
         }
     }
 }
